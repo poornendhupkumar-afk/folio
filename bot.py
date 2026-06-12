43145ae47f0e9ff912f22d1f2ed9a116b569dec1
poornendhupkumar-afk <poornendhupkumar@gmail.com>
2026-06-12 08:53:47 +0530

    Add email notification feature

--- bot.py
+++ bot.py
@@ -1,3 +1,6 @@
+import smtplib
+import os
+from email.message import EmailMessage
 import requests
 from datetime import date
 def get_weather(city="Thiruvananthapuram"):
@@ -57,11 +60,29 @@ def build_summary():
         """
         return summary
 def run():
-        summary = build_summary()
-        print(summary)
-        with open("daily_summary.txt", "w" , encoding="utf-8") as f:
-            f.write(summary)
-            print("pulse ran successfully")
+    summary = build_summary()
 
+    print(summary)
+
+    with open("daily_summary.txt", "w", encoding="utf-8") as f:
+        f.write(summary)
+
+    send_email(summary)
+
+    print("pulse ran successfully")
+def send_email(summary):
+    sender = os.getenv("EMAIL_ADDRESS")
+    password = os.getenv("EMAIL_PASSWORD")
+
+    msg = EmailMessage()
+    msg["Subject"] = "Pulse Daily Summary"
+    msg["From"] = sender
+    msg["To"] = sender
+
+    msg.set_content(summary)
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+        smtp.login(sender, password)
+        smtp.send_message(msg)
 if __name__ == "__main__":  
     run()
